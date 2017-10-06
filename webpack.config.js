1cbadd300b013c834135d09539211bd641a81030
viktor.romanyuk <viktor.romanyuk@aisnovations.com>
2017-10-06 15:08:21 +0300

    Output Management

--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-        entry: './src/index.js',
-        output: {
-            filename: 'bundle.js',
-            path: path.resolve(__dirname, 'dist')
-        },
-       module: {
-         rules: [
-               {
-                 test: /\.css$/,
-                 use: [
-                   'style-loader',
-                   'css-loader'
-                 ]
-           }
-         ]
-       }
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js'
+    },
+    plugins: [
+        new CleanWebpackPlugin(['dist']),
+        new HtmlWebpackPlugin({
+            title: 'Output Management'
+        })
+    ],
+    output: {
+        filename: '[name].bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    }
 };
