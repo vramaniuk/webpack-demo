4402bcb02b3698828cb91045a7a92f571ce67c15
viktor.romanyuk <viktor.romanyuk@aisnovations.com>
2017-10-06 18:30:57 +0300

    Asset Management(added missed tasks)

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
 import _ from 'lodash';
-import printMe from './print.js';
+import './style.css';
+import Icon from './icon.png';
+import Data from './data.xml';
 
 function component() {
     var element = document.createElement('div');
-    var btn = document.createElement('button');
 
+    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+    element.classList.add('hello');
 
-    btn.innerHTML = 'Click me and check the console!';
-    btn.onclick = printMe;
+    // Add the image to our existing div.
+    var myIcon = new Image();
+    myIcon.src = Icon;
 
-    element.appendChild(btn);
-
+    element.appendChild(myIcon);
+    console.log(Data);
     return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,25 +1,44 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CleanWebpackPlugin = require('clean-webpack-plugin');
 
 module.exports = {
-    entry: {
-        app: './src/index.js',
-        print: './src/print.js'
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: './dist'
-    },
-    plugins: [
-        new CleanWebpackPlugin(['dist']),
-        new HtmlWebpackPlugin({
-            title: 'Output Management'
-        })
-    ],
+    entry: './src/index.js',
     output: {
-        filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist'),
-        publicPath: '/'
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist')
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: [
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test: /\.(png|svg|jpg|gif)$/,
+                use: [
+                    'file-loader?emitFile=false&name=[path][name].[ext]'
+                ]
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    'file-loader?emitFile=false&name=[path][name].[ext]'
+                ]
+            },
+            {
+                test: /\.(csv|tsv)$/,
+                use: [
+                    'csv-loader'
+                ]
+            },
+            {
+                test: /\.xml$/,
+                use: [
+                    'xml-loader'
+                ]
+            }
+        ]
     }
 };
