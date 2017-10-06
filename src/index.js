bfa2722f4276b5f747ea1fc45baed40072a40e2f
viktor.romanyuk <viktor.romanyuk@aisnovations.com>
2017-10-06 19:12:08 +0300

    Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 import _ from 'lodash';
-import './style.css';
-import Icon from './icon.png';
-import Data from './data.xml';
+import printMe from './print.js';
+import './styles.css';
 
 function component() {
     var element = document.createElement('div');
+    var btn = document.createElement('button');
 
-    // Lodash, now imported by this script
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.classList.add('hello');
 
-    // Add the image to our existing div.
-    var myIcon = new Image();
-    myIcon.src = Icon;
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;  // onclick event is bind to the original printMe function
 
-    element.appendChild(myIcon);
-    console.log(Data);
+    element.appendChild(btn);
+
     return element;
 }
 
-document.body.appendChild(component());
+ let element = component(); // Store the element to re-render on print.js changes
+ document.body.appendChild(element);
+
+if (module.hot) {
+    module.hot.accept('./print.js', function() {
+        console.log('Accepting the updated printMe module!');
+             document.body.removeChild(element);
+             element = component(); // Re-render the "component" to update the click handler
+             document.body.appendChild(element);
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,32 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 module.exports = {
-    entry: './src/index.js',
+        entry: {
+            app: './src/index.js'
+        },
+        devtool: 'inline-source-map',
+        devServer: {
+            contentBase: './dist',
+            hot: true
+        },
+       module: {
+         rules: [
+               {
+                 test: /\.css$/,
+                 use: ['style-loader', 'css-loader']
+           }
+         ]
+       },
+plugins: [
+    new HtmlWebpackPlugin({
+        title: 'Hot Module Replacement'
+    }),
+    new webpack.HotModuleReplacementPlugin()
+],
     output: {
-        filename: 'bundle.js',
+    filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-            },
-            {
-                test: /\.(png|svg|jpg|gif)$/,
-                use: [
-                    'file-loader?emitFile=false&name=[path][name].[ext]'
-                ]
-            },
-            {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                    'file-loader?emitFile=false&name=[path][name].[ext]'
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            }
-        ]
-    }
+}
 };
