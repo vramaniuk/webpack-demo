183b84e349ea47d164d52b948a76e50e89aa1c0e
vramaniuk <astyanax@mail.ru>
2017-10-07 19:52:15 +0300

    Tree Shaking

--- webpack.config.js
+++ webpack.config.js
@@ -1,32 +1,13 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
 
 module.exports = {
-        entry: {
-            app: './src/index.js'
-        },
-        devtool: 'inline-source-map',
-        devServer: {
-            contentBase: './dist',
-            hot: true
-        },
-       module: {
-         rules: [
-               {
-                 test: /\.css$/,
-                 use: ['style-loader', 'css-loader']
-           }
-         ]
-       },
-plugins: [
-    new HtmlWebpackPlugin({
-        title: 'Hot Module Replacement'
-    }),
-    new webpack.HotModuleReplacementPlugin()
-],
+    entry: './src/index.js',
     output: {
-    filename: '[name].bundle.js',
-        path: path.resolve(__dirname, 'dist')
-}
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    // plugins: [
+    //     new UglifyJSPlugin(),
+    // ],
 };
