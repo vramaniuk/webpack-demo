c0fc4ae8fbeac3923adcf3f021eca8fbd91675e5
viktor.romanyuk <viktor.romanyuk@aisnovations.com>
2017-10-09 10:40:13 +0300

    Authoring Libraries

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 import _ from 'lodash';
-import Print from './print';
+import numRef from './ref.json';
 
-function component() {
-    var element = document.createElement('div');
+export function numToWord(num) {
+    return _.reduce(numRef, (accum, ref) => {
+        return ref.num === num ? ref.word : accum;
+    }, '');
+};
 
-    // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-    element.onclick = Print.bind(null, 'Hello webpack!');
-
-    return element;
-}
-
-document.body.appendChild(component());
+export function wordToNum(word) {
+    return _.reduce(numRef, (accum, ref) => {
+        return ref.word === word && word.toLowerCase() ? ref.num : accum;
+    }, -1);
+};
